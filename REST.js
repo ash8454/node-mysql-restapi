e53d7b3dd8f378a2210148fbad8dde7c8555e8e1
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-03-05 01:56:31 -0500

    Adding heroku config username|pwd

--- REST.js
+++ REST.js
@@ -4,11 +4,6 @@ function REST_ROUTER(router,connection,md5) {
     var self = this;
     self.handleRoutes(router,connection,md5);
 }
-//
-// function REST_ROUTER(router,connection,md5) {
-//     var self = this;
-//     self.handleRoutes(router,connection,md5);
-// }
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.get("/",function(req,res){
